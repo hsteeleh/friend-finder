bed71c7b90dcbc03828cf628a13844033f249c2b
Hannah <37351110+hsteeleh@users.noreply.github.com>
2018-12-27 11:37:25 -0500

    Add files via upload

---
+++ data/friends.js
@@ -0,0 +1,165 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Maned Wolf',
+		photo: 'https://i.ytimg.com/vi/3SgsPBzboGk/maxresdefault.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Commersons Dolphin',
+		photo: 'http://images.swoop-patagonia.com/768x0x50/Commersons_Dolphins_FS.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Okapi',
+		photo: 'https://cdn.britannica.com/s:500x350/04/152204-004-38B4CFBC.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Ay Ay',
+		photo: 'http://ichef.bbci.co.uk/naturelibrary/images/ic/credit/640x395/a/ay/aye-aye/aye-aye_1.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Musk Deer',
+		photo: 'https://3.bp.blogspot.com/-iO3QF9LKlUo/WUp8huGcGnI/AAAAAAAAhV4/dJGVJfu4cbIExQ_n-V87Fk_rdTJIvCuNwCLcBGAs/s1600/musk-deer.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'California Red Sided Garter Snake',
+		photo: 'http://www.thirdeyeherp.com/assets/images/22-california-red-sided-garter-snake-0504-2000x1435-29-2000x1435-69.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Superb Bird of Paradise',
+		photo: 'https://img.purch.com/h/1000/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA5Ny83Mzcvb3JpZ2luYWwvYmlyZC1vZi1wYXJhZGlzZS0wMDJfQkJDLmpwZw==',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Channel Billed Toucan',
+		photo: 'https://c1.staticflickr.com/8/7676/16852881673_d6e94f713b_c.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Genet',
+		photo: 'https://www.worldlifeexpectancy.com/images/a/w/b/genetta-servalina/genetta-servalina.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Honduran White Bat',
+		photo: 'http://www.animalspot.net/wp-content/uploads/2016/11/Honduran-White-Bat.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
